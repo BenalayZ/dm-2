affb537134fcbfbfa5c95d02be664d834d383dac
BenalayZ <36934238+BenalayZ@users.noreply.github.com>
2021-12-02 15:19:27 -0600

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,9 @@ module Dm2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+config.action_mailer.delivery_method = :postmark
+    Config.action_mailer.postmark_Settings = {
+        api_token: Rails.application.crednetials.postmark_api_token}
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
